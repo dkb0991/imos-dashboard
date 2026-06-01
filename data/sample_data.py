2f5cb1f5ae8d482efbfc2872ac08e7a7cab96528
dkb0991 <bothradeep@yahoo.com>
2026-06-02 01:41:51 +0530

    Create sample_data.py

---
+++ data/sample_data.py
@@ -0,0 +1,17 @@
+sample_inputs = {
+    "structural_health": 75,
+    "liquidity": 70,
+    "risk_appetite": 65,
+    "momentum": 60,
+    "sentiment": 55,
+    "crash_risk": 70,
+    "ipo_heat": 60,
+    "valuation": 55,
+    "drawdown": 45,
+    "vix": 60,
+    "ten_year_yield": 70,
+    "real_yield": 65,
+    "rbi_cycle": 60,
+    "yield_curve": 70,
+    "credit_spread": 55
+}
